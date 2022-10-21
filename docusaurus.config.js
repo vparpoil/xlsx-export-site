9f4868916b25724177b7362f5e0f6409e74f5898
Victor Parpoil <vparpoil@gmail.com>
2022-10-21 11:39:38 +0200

    + Add workflow to deploy to github page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,20 +4,23 @@
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
+const organizationName = "vparpoil";
+const projectName = "xlsx-export-site";
+
 /** @type {import('@docusaurus/types').Config} */
 const config = {
     title: 'JSON to XLSX',
     tagline: 'By Developpers for Developpers. Generate real Excel files with templating',
-    url: 'https://your-docusaurus-test-site.com',
-    baseUrl: '/',
+    url: `https://${organizationName}.github.io`,
+    baseUrl: `/${projectName}/`,
     onBrokenLinks: 'throw',
     onBrokenMarkdownLinks: 'warn',
     favicon: 'img/favicon.ico',
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
-    organizationName: 'allohouston', // Usually your GitHub org/user name.
-    projectName: 'jsontoxlsx', // Usually your repo name.
+    organizationName: organizationName, // Usually your GitHub org/user name.
+    projectName: projectName, // Usually your repo name.
 
     // Even if you don't use internalization, you can use this field to set useful
     // metadata like html lang. For example, if your site is Chinese, you may want
@@ -37,14 +40,14 @@ const config = {
                     // Please change this to your repo.
                     // Remove this to remove the "edit this page" links.
                     editUrl:
-                        'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+                        `https://github.com/${organizationName}/${projectName}/tree/main/`,
                 },
                 blog: {
                     showReadingTime: true,
                     // Please change this to your repo.
                     // Remove this to remove the "edit this page" links.
                     editUrl:
-                        'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+                        `https://github.com/${organizationName}/${projectName}/tree/main/`,
                 },
                 theme: {
                     customCss: require.resolve('./src/css/custom.css'),
